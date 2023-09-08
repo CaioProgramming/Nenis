6558ac422c912f3c38223b45c00c32399344d55d
Caio Alves Ferreira <caioalvesferreiracontato@gmail.com>
2023-09-07 22:51:05 -0300

    implementing tags on new activity using now struct to save activities(need to make custom cell next)

--- Nenis/features/activities/NewActivityViewController.swift
+++ Nenis/features/activities/NewActivityViewController.swift
@@ -8,28 +8,62 @@
 import UIKit
 
 protocol ActivityProtocol: AnyObject {
-    func retrieveActivity(activityDescription: String)
+    func retrieveActivity(activityDescription: Activity)
 }
 
 class NewActivityViewController: UIViewController {
 
     var activtyProtocol: ActivityProtocol?
+    var activityType = ActivityType.bath
     @IBOutlet weak var textField: UITextField!
     
+    @IBOutlet weak var activityTypeButton: UIButton!
     @IBOutlet weak var saveButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+       
+        activityTypeButton.setTitle(activityType.emoji, for: .normal)
+        activityTypeButton.menu = UIMenu(
+            title: "Atividade do bebê",
+            identifier: UIMenu.Identifier(rawValue: "activityMenu"),
+            children: getMenuForTypes()
+        )
+        activityTypeButton.showsMenuAsPrimaryAction = true
+        activityTypeButton.changesSelectionAsPrimaryAction = true
         // Do any additional setup after loading the view.
     }
     
+    func getMenuForTypes() -> [UIAction] {
+        var actions = [UIAction]()
+        let activityClosure = {(action: UIAction) in
+            self.activityType = self.getActivityForValue(identifier: action.identifier.rawValue)
+            self.updateActivityType()
+        }
+        ActivityType.allCases.forEach({ type in
+            actions.append(UIAction(title: type.title, identifier: UIAction.Identifier(type.description),state: .on, handler: activityClosure))
+        })
+        
+        return actions
+    }
+    
+    func updateActivityType() {
+        activityTypeButton.setTitle(activityType.emoji, for: .normal)
+    }
+    
     @IBAction func saveActivityTap(_ sender: UIButton) {
-        var text = textField.text
+        let text = textField.text
         if(textField.hasText) {
-            activtyProtocol?.retrieveActivity(activityDescription: text ?? "")
+            activtyProtocol?.retrieveActivity(activityDescription: Activity(description: text!, type: activityType, time: Date.now))
             self.dismiss(animated: true)
         }
         
+    }
+    
+    
+    func getActivityForValue(identifier: String) -> ActivityType {
+        return ActivityType.allCases.first(where: { type in
+            type.description == identifier
+        }) ?? .bath
     }
     
     @IBAction func textEndEdit(_ sender: UITextField) {
--- Nenis/features/home/HomeViewController.swift
+++ Nenis/features/home/HomeViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class HomeViewController: UIViewController, ActivityProtocol{
     
-    var activities = [String]();
+    var activities = [Activity]();
     @IBOutlet weak var activityTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +21,7 @@ class HomeViewController: UIViewController, ActivityProtocol{
     @IBAction func createNewActivity(_ sender: UIButton) {
         performSegue(withIdentifier: "NewActivitySegue", sender: self)
     }
-    func retrieveActivity(activityDescription: String) {
+    func retrieveActivity(activityDescription: Activity) {
         activities.append(activityDescription)
         print("new activity -> \(activityDescription)")
         print(activities.description)
@@ -51,9 +51,21 @@ extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "activityCell", for: indexPath)
         let activity = activities[indexPath.row]
-        cell.textLabel?.text = activity
+        cell.textLabel?.text = activity.description
+        cell.detailTextLabel?.text = formatDate(date: activity.time)
+        cell.imageView?.image =  activity.type.cellImage
+        cell.imageView?.tintColor = activity.type.imageTint
         return cell
     }
+    
+    func formatDate(date: Date) -> String{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+        return dateFormatter.string(from: date)
+    }
 }
+
+ 
 
  
