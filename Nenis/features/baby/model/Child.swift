c634c68b36ad6f4196d613fd011db8537179fdd2
Caio Alves Ferreira <caioalvesferreiracontato@gmail.com>
2023-11-02 00:33:31 -0300

    better UI for new child form using now longdate time to save filename to avoid overrided files

--- Nenis/features/baby/model/Child.swift
+++ Nenis/features/baby/model/Child.swift
@@ -7,19 +7,41 @@
 
 import Foundation
 import FirebaseFirestoreSwift
+import UIKit
 
 public struct Child: Codable {
     @DocumentID var id: String?
     let name: String
     let birthDate: Date
     let photo: String
+    var gender: String
     var tutors: [String]
     var actions: [Action]
     enum CodingKeys: String, CodingKey {
         case name
         case birthDate
         case photo
+        case gender
         case tutors
         case actions
+    }
+}
+
+enum Gender : Codable, CaseIterable {
+    case boy, girl
+    
+    var description: String { get { return "\(self)" } }
+
+    
+    var color: UIColor {
+        get {
+            switch self {
+                
+            case .boy:
+                #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+            case .girl:
+                #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+            }
+        }
     }
 }
